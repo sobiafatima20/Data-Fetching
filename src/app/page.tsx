83629287c0038c70ec44bfd2b029d9e86829df2b
Sobia Fatima <sobia.fatima2020@gmail.com>
2024-12-22 16:43:50 +0500

    Data Fetching Client server side Data fetching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import ServerSide from "./Server-Side/page";
+import ClientSide from "./Client-Side/page";
 
 export default function Home() {
   return (
     <>
+    <ServerSide />
+    <ClientSide/>
     </>
   );
 }
